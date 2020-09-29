6bc607cbd9dae4e3e1b9e3cf7cc88a6e79e1ba32
Roy Hung <43883872+arroy114@users.noreply.github.com>
2020-09-29 10:49:54 +0200

    Adding index page

--- src/main/java/wai/spring5recipeapp/Spring5RecipeAppApplication.java
+++ src/main/java/wai/spring5recipeapp/Spring5RecipeAppApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Spring5RecipeAppApplication {
 
+    //testing
+
     public static void main(String[] args) {
         SpringApplication.run(Spring5RecipeAppApplication.class, args);
     }
